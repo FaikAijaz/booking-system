6f3dde58b5ae7fce6bbe00e39978f83df13ab446
Faik <faikaijaz635@gmail.com>
2023-03-08 18:56:15 +0530

    displaying Json data

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
-print("hello")
-#ansjvbdajba;jn;skjb;jkskdnk;x
-#dsd
+import json, html
+from flask import Flask, jsonify
+
+
+
+
+app = Flask(__name__)
+
+# read file
+with open('booking-system\data.json', 'r') as myfile:
+    data = json.load(myfile)
+#
+
+@app.route("/")
+def index():
+    return jsonify(data["venue"])
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
