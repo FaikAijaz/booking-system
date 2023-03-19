87d98deea69dcbd5e63fb79609825b629c7e60eb
Faik <faikaijaz635@gmail.com>
2023-03-19 12:55:45 +0530

    CRUD on venues and shows

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ with open('booking-system\data.json', 'r') as myfile:
 
 @app.route("/", methods=["GET"])
 def index():
-    return jsonify(data["venue"])
+    return "Booking system home page"
 
 @app.route("/login", methods=["GET","POST"])
 
@@ -41,6 +41,102 @@ def login():
         
     elif(request.method=="GET"):
         return jsonify({})
+
+@app.route("/venues", methods=["GET","POST","DELETE","PUT"])
+def venue():
+    #READ VENUE
+    if(request.method=="GET"):
+        venues=[]
+        for i in data["venue"]:
+
+            venues.append(i)
+        
+
+        return venues
+    #CREATE VENUE
+    elif(request.method=="POST"):
+        body=request.get_json()
+        venues=data["venue"]
+        
+        venues.append(body)
+        with open("\data.json", 'w') as f:
+            json.dump(data, f, indent=4)
+        return venues  
+    #DELETE VENUE  
+    elif(request.method=="DELETE"):
+        body=request.get_json()
+        venues=data["venue"]
+        
+        index=venues.index(body)
+        venues.pop(index)
+        
+        with open("\data.json", 'w') as f:
+            json.dump(data, f, indent=4)
+        return venues
+    #UPDATE VENUE
+    elif(request.method=="PUT"):
+        body=request.get_json()
+        venues=data["venue"]
+        index=venues.index(body[0])
+        venues.pop(index)
+        venues.append(body[1])
+
+        
+        
+        
+        with open("\data.json", 'w') as f:
+            json.dump(data, f, indent=4)
+        return venues
+    
+
+
+
+@app.route("/shows", methods=["GET","POST","DELETE","PUT"])
+def show():
+     #READ SHOW
+    if(request.method=="GET"):
+        shows=[]
+        for i in data["show"]:
+
+            shows.append(i)
+        
+
+        return shows
+    #CREATE SHOW
+    elif(request.method=="POST"):
+        body=request.get_json()
+        shows=data["venue"]
+        
+        shows.append(body)
+        with open("\data.json", 'w') as f:
+            json.dump(data, f, indent=4)
+        return shows  
+    #DELETE SHOW 
+    elif(request.method=="DELETE"):
+        body=request.get_json()
+        shows=data["show"]
+        
+        index=shows.index(body)
+        shows.pop(index)
+        
+        with open("\data.json", 'w') as f:
+            json.dump(data, f, indent=4)
+        return shows
+    #UPDATE SHOW
+    elif(request.method=="PUT"):
+        body=request.get_json()
+        shows=data["show"]
+        index=shows.index(body[0])
+        shows.pop(index)
+        shows.append(body[1])
+
+        
+        
+        
+        with open("\data.json", 'w') as f:
+            json.dump(data, f, indent=4)
+        return shows
+    
 
 
 
--- test.py
+++ test.py
@@ -3,6 +3,6 @@ with open('data.json', 'r') as myfile:
     data = json.load(myfile)
 
 #print((data["user"][0]["username1"]))
-deta=(data["user"][0:])
-
-print(deta)
+for i in data["venue"]:
+        venues=print(i)
+        
